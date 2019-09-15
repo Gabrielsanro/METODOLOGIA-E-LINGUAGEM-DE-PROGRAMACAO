57ff0860d0f2bd3f8ac477b25f452f34b500cc14
Gabrielsanro <47647925+Gabrielsanro@users.noreply.github.com>
2019-09-15 11:02:03 -0300

    Add files via upload

---
+++ AumentoSalario.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AumentoSalario {
+ 
+	    public static void main(String[] args) {
+	        double salario;
+	        double percentual;
+	        Scanner scan = new Scanner(System.in);
+	        System.out.println("Digite aqui o salário antigo: ");
+	        salario = scan.nextDouble();
+	        System.out.println("Digite aqui o percentual: ");
+	        percentual = scan.nextDouble();
+	        System.out.printf("O novo salário é igual a : R$%.2f", (salario += salario * percentual/100));
+	    }
+	}
---
+++ Numeros.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Numeros {
+
+	
+
+
+	    public static void main(String[] args) {
+
+	        Scanner scanner = new Scanner(System.in);
+
+	        for(int i = 0; i < 10; i++){
+	            System.out.println("Digite aqui um número: ");
+	            System.out.println(scanner.nextInt() % 2 == 0 ? "Número par!" : "Número ímpar!");
+	        }
+	    }
+	}
